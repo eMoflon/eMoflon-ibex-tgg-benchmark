a53bc22d4298e3ef4e2836da1da15c06ddf865e2
Andre Lehmann <aisberg@posteo.de>
2019-08-20 10:46:49 +0200

    Update methods to load and convert to JSON

--- src/main/java/org/emoflon/ibex/tgg/benchmark/model/PluginPreferences.java
+++ src/main/java/org/emoflon/ibex/tgg/benchmark/model/PluginPreferences.java
@@ -1,27 +1,33 @@
 package org.emoflon.ibex.tgg.benchmark.model;
 
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 
+import java.lang.IllegalArgumentException;
+
+import org.apache.logging.log4j.spi.StandardLevel;
 import org.emoflon.ibex.tgg.benchmark.runner.report.ReportFileType;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import ch.qos.logback.*;
 
 public class PluginPreferences implements IPreferences {
 
     // general
     private final StringProperty benchmarkPreferencesFileName;
-    // TODO: implement LogLevel
+    private final ObjectProperty<StandardLevel> logLevel;
 
     // benchmark
     private final IntegerProperty maxMemorySize;
@@ -29,7 +35,7 @@ public class PluginPreferences implements IPreferences {
 
     // report
     private final StringProperty reportFilePath;
-    private final StringProperty reportFileType;
+    private final ObjectProperty<ReportFileType> reportFileType;
     private final BooleanProperty includeErrors;
 
     // default values
@@ -48,6 +54,7 @@ public class PluginPreferences implements IPreferences {
     public PluginPreferences() {
         // general
         benchmarkPreferencesFileName = new SimpleStringProperty(".tgg-benchmark.json"); // relative to project
+        logLevel = new SimpleObjectProperty<>(StandardLevel.ALL);
 
         // benchmark
         repetitions = new SimpleIntegerProperty(3);
@@ -55,7 +62,7 @@ public class PluginPreferences implements IPreferences {
 
         // report
         reportFilePath = new SimpleStringProperty("{workspace_path}/{Y}-{M}-{D} {h}-{m} TGGBenchmarkReport");
-        reportFileType = new SimpleStringProperty(ReportFileType.EXCEL.toString());
+        reportFileType = new SimpleObjectProperty<>(ReportFileType.EXCEL);
         includeErrors = new SimpleBooleanProperty(true);
 
         // defaults
@@ -77,35 +84,102 @@ public class PluginPreferences implements IPreferences {
 
         JsonObject general = data.getJsonObject("general");
         if (general != null) {
-            setBenchmarkPreferencesFileName(
-                    data.getString("benchmarkPreferencesFileName", getBenchmarkPreferencesFileName()));
-            setRepetitions(general.getInt("repetitions", getRepetitions()));
+            String benchmarkPreferencesFileName = general.getString("benchmarkPreferencesFileName", getBenchmarkPreferencesFileName());
+            setBenchmarkPreferencesFileName(benchmarkPreferencesFileName != null ? benchmarkPreferencesFileName : getBenchmarkPreferencesFileName());
+            try {
+                setLogLevel(StandardLevel.valueOf(general.getString("logLevel", getLogLevel().toString())));
+            } catch (IllegalArgumentException e) {
+                // keep default
+            }
         }
 
         JsonObject benchmark = data.getJsonObject("benchmark");
         if (benchmark != null) {
-
+            int maxMemorxSize = benchmark.getInt("maxMemorySize", getMaxMemorySize());
+            setMaxMemorySize(maxMemorxSize > 0 ? maxMemorxSize : getMaxMemorySize());         
+            int repetitions = benchmark.getInt("repetitions", getRepetitions());
+            setRepetitions(repetitions > 0 ? repetitions : getRepetitions());
         }
 
         JsonObject report = data.getJsonObject("report");
         if (report != null) {
-
+            String reportFilePath = general.getString("filePath", getReportFilePath());
+            setReportFilePath(reportFilePath != null ? reportFilePath : getReportFilePath());
+            try {
+                setReportFileType(ReportFileType.valueOf(general.getString("fileType", getBenchmarkPreferencesFileName())));
+            } catch (IllegalArgumentException e) {
+                // keep default
+            }
+            setIncludeErrors(general.getBoolean("includeErrors", isIncludeErrors()));
         }
 
         JsonObject defaults = data.getJsonObject("defaults");
         if (defaults != null) {
-
+            int defaultTimeout = benchmark.getInt("timeout", getDefaultTimeout());
+            setDefaultTimeout(defaultTimeout > 0 ? defaultTimeout : getDefaultTimeout());         
+            JsonArray modelSizesArray = benchmark.getJsonArray("modelSizes");
+            if (modelSizesArray != null) {
+                ObservableList<Integer> modelSizes = FXCollections.observableArrayList();
+                for (int i = 0; i < modelSizesArray.size(); i++) {
+                    try {
+                        modelSizes.add(modelSizesArray.getInt(i));
+                    } catch (ClassCastException e) {
+                        // ignore
+                    }
+                }
+                setDefaultModelSizes(modelSizes);
+            }
+            setDefaultModelgenIncludeReport(general.getBoolean("modelgenIncludeReport", isDefaultModelgenIncludeReport()));
+            setDefaultInitialFwdActive(general.getBoolean("initialFwdActive", isDefaultInitialFwdActive()));
+            setDefaultInitialBwdActive(general.getBoolean("initialBwdActive", isDefaultInitialBwdActive()));
+            setDefaultFwdActive(general.getBoolean("fwdActive", isDefaultFwdActive()));
+            setDefaultBwdActive(general.getBoolean("bwdActive", isDefaultBwdActive()));
+            setDefaultFwdOptActive(general.getBoolean("fwdOptActive", isDefaultFwdOptActive()));
+            setDefaultBwdOptActive(general.getBoolean("bwdOptActive", isDefaultBwdOptActive()));
+            setDefaultCcActive(general.getBoolean("ccActive", isDefaultCcActive()));
+            setDefaultCoActive(general.getBoolean("coActive", isDefaultCoActive()));
         }
     }
 
     @Override
     public JsonObject toJson() {
-        JsonObject preferences = Json.createObjectBuilder().add("general",
-                Json.createObjectBuilder().add("benchmarkPreferencesFileName", getBenchmarkPreferencesFileName())
-                        .add("input", Json.createObjectBuilder().build())
-                        .add("report", Json.createObjectBuilder().build())
-                        .add("repetitions", getRepetitions())
-                        .add("operationalizations", Json.createObjectBuilder()).build())
+
+        JsonArrayBuilder modelSizesBuilder = Json.createArrayBuilder();
+        for (Integer integer : getDefaultModelSizes()) {
+            modelSizesBuilder.add(integer);
+        }
+
+        JsonObject preferences = Json.createObjectBuilder()
+                .add("general",
+                    Json.createObjectBuilder()
+                            .add("benchmarkPreferencesFileName", getBenchmarkPreferencesFileName())
+                            .add("logLevel", getLogLevel().toString())
+                            .build())
+                .add("benchmark",
+                    Json.createObjectBuilder()
+                            .add("maxMemorySize", getMaxMemorySize())
+                            .add("repetitions", getRepetitions())
+                            .build())
+                .add("report",
+                    Json.createObjectBuilder()
+                            .add("filePath", getReportFilePath())
+                            .add("fileType", getReportFileType().toString())
+                            .add("includeErrors", isIncludeErrors())
+                            .build())
+                .add("defaults",
+                    Json.createObjectBuilder()
+                            .add("timeout", getBenchmarkPreferencesFileName())
+                            .add("modelSizes", modelSizesBuilder.build())
+                            .add("modelgenIncludeReport", isDefaultModelgenIncludeReport())
+                            .add("initialFwdActive", isDefaultInitialFwdActive())
+                            .add("initialBwdActive", isDefaultInitialBwdActive())
+                            .add("fwdActive", isDefaultFwdActive())
+                            .add("bwdActive", isDefaultBwdActive())
+                            .add("fwdOptActive", isDefaultFwdOptActive())
+                            .add("bwdOptActive", isDefaultBwdOptActive())
+                            .add("ccActive", isDefaultCcActive())
+                            .add("coActive", isDefaultCoActive())
+                            .build())
                 .build();
 
         return preferences;
@@ -159,18 +233,6 @@ public class PluginPreferences implements IPreferences {
         this.includeErrorsProperty().set(includeErrors);
     }
 
-    public final StringProperty reportFileTypeProperty() {
-        return this.reportFileType;
-    }
-
-    public final ReportFileType getReportFileType() {
-        return ReportFileType.valueOf(this.reportFileTypeProperty().get());
-    }
-
-    public final void setReportFileType(final String reportFileType) {
-        this.reportFileTypeProperty().set(reportFileType);
-    }
-
     public final IntegerProperty repetitionsProperty() {
         return this.repetitions;
     }
@@ -186,165 +248,159 @@ public class PluginPreferences implements IPreferences {
     public final IntegerProperty defaultTimeoutProperty() {
         return this.defaultTimeout;
     }
-    
 
     public final int getDefaultTimeout() {
         return this.defaultTimeoutProperty().get();
     }
-    
 
     public final void setDefaultTimeout(final int defaultTimeout) {
         this.defaultTimeoutProperty().set(defaultTimeout);
     }
-    
 
     public final ListProperty<Integer> defaultModelSizesProperty() {
         return this.defaultModelSizes;
     }
-    
 
     public final ObservableList<Integer> getDefaultModelSizes() {
         return this.defaultModelSizesProperty().get();
     }
-    
 
     public final void setDefaultModelSizes(final ObservableList<Integer> defaultModelSizes) {
         this.defaultModelSizesProperty().set(defaultModelSizes);
     }
-    
 
     public final BooleanProperty defaultModelgenIncludeReportProperty() {
         return this.defaultModelgenIncludeReport;
     }
-    
 
     public final boolean isDefaultModelgenIncludeReport() {
         return this.defaultModelgenIncludeReportProperty().get();
     }
-    
 
     public final void setDefaultModelgenIncludeReport(final boolean defaultModelgenIncludeReport) {
         this.defaultModelgenIncludeReportProperty().set(defaultModelgenIncludeReport);
     }
-    
 
     public final BooleanProperty defaultInitialFwdActiveProperty() {
         return this.defaultInitialFwdActive;
     }
-    
 
     public final boolean isDefaultInitialFwdActive() {
         return this.defaultInitialFwdActiveProperty().get();
     }
-    
 
     public final void setDefaultInitialFwdActive(final boolean defaultInitialFwdActive) {
         this.defaultInitialFwdActiveProperty().set(defaultInitialFwdActive);
     }
-    
 
     public final BooleanProperty defaultInitialBwdActiveProperty() {
         return this.defaultInitialBwdActive;
     }
-    
 
     public final boolean isDefaultInitialBwdActive() {
         return this.defaultInitialBwdActiveProperty().get();
     }
-    
 
     public final void setDefaultInitialBwdActive(final boolean defaultInitialBwdActive) {
         this.defaultInitialBwdActiveProperty().set(defaultInitialBwdActive);
     }
-    
 
     public final BooleanProperty defaultFwdActiveProperty() {
         return this.defaultFwdActive;
     }
-    
 
     public final boolean isDefaultFwdActive() {
         return this.defaultFwdActiveProperty().get();
     }
-    
 
     public final void setDefaultFwdActive(final boolean defaultFwdActive) {
         this.defaultFwdActiveProperty().set(defaultFwdActive);
     }
-    
 
     public final BooleanProperty defaultBwdActiveProperty() {
         return this.defaultBwdActive;
     }
-    
 
     public final boolean isDefaultBwdActive() {
         return this.defaultBwdActiveProperty().get();
     }
-    
 
     public final void setDefaultBwdActive(final boolean defaultBwdActive) {
         this.defaultBwdActiveProperty().set(defaultBwdActive);
     }
-    
 
     public final BooleanProperty defaultFwdOptActiveProperty() {
         return this.defaultFwdOptActive;
     }
-    
 
     public final boolean isDefaultFwdOptActive() {
         return this.defaultFwdOptActiveProperty().get();
     }
-    
 
     public final void setDefaultFwdOptActive(final boolean defaultFwdOptActive) {
         this.defaultFwdOptActiveProperty().set(defaultFwdOptActive);
     }
-    
 
     public final BooleanProperty defaultBwdOptActiveProperty() {
         return this.defaultBwdOptActive;
     }
-    
 
     public final boolean isDefaultBwdOptActive() {
         return this.defaultBwdOptActiveProperty().get();
     }
-    
 
     public final void setDefaultBwdOptActive(final boolean defaultBwdOptActive) {
         this.defaultBwdOptActiveProperty().set(defaultBwdOptActive);
     }
-    
 
     public final BooleanProperty defaultCcActiveProperty() {
         return this.defaultCcActive;
     }
-    
 
     public final boolean isDefaultCcActive() {
         return this.defaultCcActiveProperty().get();
     }
-    
 
     public final void setDefaultCcActive(final boolean defaultCcActive) {
         this.defaultCcActiveProperty().set(defaultCcActive);
     }
-    
 
     public final BooleanProperty defaultCoActiveProperty() {
         return this.defaultCoActive;
     }
-    
 
     public final boolean isDefaultCoActive() {
         return this.defaultCoActiveProperty().get();
     }
-    
 
     public final void setDefaultCoActive(final boolean defaultCoActive) {
         this.defaultCoActiveProperty().set(defaultCoActive);
+    }
+
+    public final ObjectProperty<StandardLevel> logLevelProperty() {
+        return this.logLevel;
+    }
+
+    public final StandardLevel getLogLevel() {
+        return this.logLevelProperty().get();
+    }
+
+    public final void setLogLevel(final StandardLevel logLevel) {
+        this.logLevelProperty().set(logLevel);
+    }
+
+    public final ObjectProperty<ReportFileType> reportFileTypeProperty() {
+        return this.reportFileType;
+    }
+    
+
+    public final ReportFileType getReportFileType() {
+        return this.reportFileTypeProperty().get();
+    }
+    
+
+    public final void setReportFileType(final ReportFileType reportFileType) {
+        this.reportFileTypeProperty().set(reportFileType);
     }
     
 
