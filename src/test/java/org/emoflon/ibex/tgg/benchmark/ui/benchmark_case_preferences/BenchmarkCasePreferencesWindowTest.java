cd6e1d2d7bdbee205bcca3a247ea3d9d8d100bc7
Andre Lehmann <aisberg@posteo.de>
2019-08-25 12:52:12 +0200

    Update test classes

--- src/test/java/org/emoflon/ibex/tgg/benchmark/ui/benchmark_case_preferences/BenchmarkCasePreferencesWindowTest.java
+++ src/test/java/org/emoflon/ibex/tgg/benchmark/ui/benchmark_case_preferences/BenchmarkCasePreferencesWindowTest.java
@@ -1,24 +1,15 @@
 package org.emoflon.ibex.tgg.benchmark.ui.benchmark_case_preferences;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.json.JsonException;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.emoflon.ibex.tgg.benchmark.Core;
-import org.emoflon.ibex.tgg.benchmark.model.BenchmarkCasePreferences;
-import org.emoflon.ibex.tgg.benchmark.model.EclipseJavaProject;
-import org.emoflon.ibex.tgg.benchmark.model.EclipseTggProject;
 import org.emoflon.ibex.tgg.benchmark.model.EclipseWorkspaceDebug;
 import org.emoflon.ibex.tgg.benchmark.model.PluginPreferences;
+import org.emoflon.ibex.tgg.benchmark.ui.Utils;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.stage.Stage;
 
 public class BenchmarkCasePreferencesWindowTest extends Application {
@@ -29,7 +20,8 @@ public class BenchmarkCasePreferencesWindowTest extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        BenchmarkCasePreferencesWindow bcpw = new BenchmarkCasePreferencesWindow(eclipseWorkspace.getTggProjects().get(0).getBenchmarkCasePreferences());
+        BenchmarkCasePreferencesWindow bcpw = new BenchmarkCasePreferencesWindow(
+                eclipseWorkspace.getTggProjects().get(0).getBenchmarkCasePreferences());
 
         try {
             bcpw.show();
@@ -39,45 +31,11 @@ public class BenchmarkCasePreferencesWindowTest extends Application {
     }
 
     public static void main(String[] args) throws JsonException, IOException {
-        // configure logger
-        Configurator.initialize(new DefaultConfiguration());
-        Configurator.setRootLevel(Level.ALL);
+        Utils.initConfiguration();
 
-        eclipseWorkspace = new EclipseWorkspaceDebug();
-        pluginPreferences = new PluginPreferences();
-
-        pluginCore = Core.createInstance(eclipseWorkspace);
-        pluginCore.setPluginPreferences(pluginPreferences);
-        
-        EclipseTggProject companyToIt = createEclipseProject("CompanyToIT",
-                new String[] { "CompanyLanguage", "ITLanguage", "CompanyToIT", });
-        BenchmarkCasePreferences companyToItBcp = new BenchmarkCasePreferences();
-        companyToItBcp.setBenchmarkCaseName(companyToIt.getName());
-        companyToIt.setBenchmarkCasePreferences(companyToItBcp);
-
-        EclipseTggProject familyToPersonas = createEclipseProject("FamiliesToPersons_V1",
-                new String[] { "SimpleFamilies", "SimplePersons", "FamiliesToPersons_V1" });
-        eclipseWorkspace.setTggProjects(FXCollections.observableArrayList(companyToIt, familyToPersonas));
-        BenchmarkCasePreferences familyToPersonasBcp = new BenchmarkCasePreferences();
-        familyToPersonasBcp.setBenchmarkCaseName(companyToIt.getName());
-        familyToPersonas.setBenchmarkCasePreferences(familyToPersonasBcp);
+        pluginPreferences = Core.getInstance().getPluginPreferences();
+        eclipseWorkspace = (EclipseWorkspaceDebug) Core.getInstance().getWorkspace();
 
         launch(args);
-    }
-
-    private static EclipseTggProject createEclipseProject(String projectName, String[] referencedProjectNames)
-            throws JsonException, IOException {
-
-        Set<EclipseJavaProject> referencedProjects = new HashSet<EclipseJavaProject>();
-        for (String referencedProjectName : referencedProjectNames) {
-            Path projectPath = eclipseWorkspace.getLocation().resolve(referencedProjectName);
-            Path outputPath = projectPath.resolve("bin");
-            referencedProjects
-                    .add(new EclipseJavaProject(referencedProjectName, projectPath, outputPath, new HashSet<>()));
-        }
-
-        Path projectPath = eclipseWorkspace.getLocation().resolve(projectName);
-        Path outputPath = projectPath.resolve("bin");
-        return new EclipseTggProject(projectName, projectPath, outputPath, referencedProjects);
     }
 }
--- src/test/java/org/emoflon/ibex/tgg/benchmark/ui/plugin_preferences/PluginPreferencesWindowTest.java
+++ src/test/java/org/emoflon/ibex/tgg/benchmark/ui/plugin_preferences/PluginPreferencesWindowTest.java
@@ -1,34 +1,23 @@
 package org.emoflon.ibex.tgg.benchmark.ui.plugin_preferences;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
 
 import javax.json.JsonException;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.emoflon.ibex.tgg.benchmark.Core;
-import org.emoflon.ibex.tgg.benchmark.model.EclipseJavaProject;
-import org.emoflon.ibex.tgg.benchmark.model.EclipseTggProject;
 import org.emoflon.ibex.tgg.benchmark.model.EclipseWorkspaceDebug;
-import org.emoflon.ibex.tgg.benchmark.model.IEclipseWorkspace;
 import org.emoflon.ibex.tgg.benchmark.model.PluginPreferences;
 import org.emoflon.ibex.tgg.benchmark.runner.report.ReportFileType;
+import org.emoflon.ibex.tgg.benchmark.ui.Utils;
 import org.emoflon.ibex.tgg.benchmark.utils.JsonUtils;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
 import javafx.stage.Stage;
 
 public class PluginPreferencesWindowTest extends Application {
 
     private static PluginPreferences pluginPreferences;
     private static EclipseWorkspaceDebug eclipseWorkspace;
-    private static Core pluginCore;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -42,23 +31,12 @@ public class PluginPreferencesWindowTest extends Application {
     }
 
     public static void main(String[] args) throws JsonException, IOException {
+        Utils.initConfiguration();
 
-        // configure logger
-        Configurator.initialize(new DefaultConfiguration());
-        Configurator.setRootLevel(Level.ALL);
+        pluginPreferences = Core.getInstance().getPluginPreferences();
+        eclipseWorkspace = (EclipseWorkspaceDebug) Core.getInstance().getWorkspace();
 
-        eclipseWorkspace = new EclipseWorkspaceDebug();
-        EclipseTggProject companyToIt = createEclipseProject("CompanyToIT",
-                new String[] { "CompanyLanguage", "ITLanguage", "CompanyToIT", });
-        EclipseTggProject familyToPersonas = createEclipseProject("FamiliesToPersons_V1",
-                new String[] { "SimpleFamilies", "SimplePersons", "FamiliesToPersons_V1" });
-        eclipseWorkspace.setTggProjects(FXCollections.observableArrayList(companyToIt, familyToPersonas));
-
-        pluginPreferences = new PluginPreferences();
         pluginPreferences.setReportFileType(ReportFileType.EXCEL);
-
-        pluginCore = Core.createInstance(eclipseWorkspace);
-        pluginCore.setPluginPreferences(pluginPreferences);
 
         System.out.println("Initial Plugin Preferences");
         System.out.println(JsonUtils.jsonToString(pluginPreferences.toJson()));
@@ -67,21 +45,5 @@ public class PluginPreferencesWindowTest extends Application {
 
         System.out.println("\n\nPlugin Preferences after closing");
         System.out.println(JsonUtils.jsonToString(pluginPreferences.toJson()));
-    }
-
-    private static EclipseTggProject createEclipseProject(String projectName, String[] referencedProjectNames)
-            throws JsonException, IOException {
-
-        Set<EclipseJavaProject> referencedProjects = new HashSet<EclipseJavaProject>();
-        for (String referencedProjectName : referencedProjectNames) {
-            Path projectPath = eclipseWorkspace.getLocation().resolve(referencedProjectName);
-            Path outputPath = projectPath.resolve("bin");
-            referencedProjects
-                    .add(new EclipseJavaProject(referencedProjectName, projectPath, outputPath, new HashSet<>()));
-        }
-
-        Path projectPath = eclipseWorkspace.getLocation().resolve(projectName);
-        Path outputPath = projectPath.resolve("bin");
-        return new EclipseTggProject(projectName, projectPath, outputPath, referencedProjects);
     }
 }
